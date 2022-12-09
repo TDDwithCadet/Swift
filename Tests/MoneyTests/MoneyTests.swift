3e3379e1cc81f6a56a50d6d7bdc56804822f83a9
zhunhe <xuzhunhe@gmail.com>
2022-12-09 12:46:31 +0900

    1장. ❌간단한 곱셈 테스트 코드 작성 (p44)

--- Tests/MoneyTests/MoneyTests.swift
+++ Tests/MoneyTests/MoneyTests.swift
@@ -2,4 +2,9 @@ import XCTest
 @testable import Money
 
 final class MoneyTests: XCTestCase {
+    func testMultiplication() throws {
+        let five = Dollar(5)
+        five.times(2)
+        XCTAssertEqual(10, five.amount)
+    }
 }

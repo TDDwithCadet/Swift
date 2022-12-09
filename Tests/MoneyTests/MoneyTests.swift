fdf343058eae01c1e66b5610f58c0e2692153535
zhunhe <xuzhunhe@gmail.com>
2022-12-09 13:26:34 +0900

    5장. ❌5CHF X 2 = 10CHF

--- Tests/MoneyTests/MoneyTests.swift
+++ Tests/MoneyTests/MoneyTests.swift
@@ -11,4 +11,10 @@ final class MoneyTests: XCTestCase {
     func testEquality() throws {
         XCTAssertTrue(Dollar(5) == Dollar(5))
     }
+
+    func testFrancMultiplication() {
+        let five = Franc(5)
+        XCTAssertEqual(Franc(10), five.times(2))
+        XCTAssertEqual(Franc(15), five.times(3))
+    }
 }

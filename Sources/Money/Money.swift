aec9cac25f5e9b6054b52d1af8392b2f7814e3ef
zhunhe <xuzhunhe@gmail.com>
2022-12-09 13:48:07 +0900

    7장. ✅Franc과 Dollar 비교하기

--- Sources/Money/Money.swift
+++ Sources/Money/Money.swift
@@ -8,7 +8,7 @@ class Money {
 
 extension Money: Equatable {
     public static func ==(lhs: Money, rhs: Money) -> Bool {
-        return lhs.amount == rhs.amount
+        return lhs.amount == rhs.amount && type(of: lhs) == type(of: rhs)
     }
 }
 

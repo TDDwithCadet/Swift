899ba0aebbbad64891cc1c780a9d89bcc50c98e3
zhunhe <xuzhunhe@gmail.com>
2022-12-09 12:49:40 +0900

    1장. ✅첫번째 테스트 완료 (p51)
    - Dollar 클래스 작성

--- Sources/Money/Money.swift
+++ Sources/Money/Money.swift
@@ -1,1 +1,11 @@
+class Dollar {
+    var amount: Int
 
+    init(_ amount: Int) {
+        self.amount = amount
+    }
+
+    func times(_ multiplier: Int) {
+        self.amount *= multiplier
+    }
+}
